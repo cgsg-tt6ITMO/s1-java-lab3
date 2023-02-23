ad38fcf41ba672c202e2d1d5e5fb60a0939bd09b
cgsg-tt6 <76934492+cgsg-tt6@users.noreply.github.com>
2023-02-24 01:34:48 +0300

    Add files via upload

--- src/exceptions/MadnessException.java
+++ src/exceptions/MadnessException.java
@@ -1,1 +1,8 @@
+package exceptions;
 
+// checked exception (is caught at compile time)
+public class MadnessException extends Exception {
+    public MadnessException(String message) {
+        super(message);
+    }
+}
---
+++ src/exceptions/NoShapeBuildingException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+// unchecked exception - RuntimeException
+public class NoShapeBuildingException extends Exception {
+
+    public NoShapeBuildingException(String message) {
+        super(message);
+    }
+}
+
